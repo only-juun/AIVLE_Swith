14788a98bcb4704724ecc1884da6bd79a8cd2a6e
leehanjun506 <sis03017@naver.com>
2023-06-24 01:36:00 +0900

    add - User Entity 생성

--- backend/src/main/java/swith/backend/domain/User.java
+++ backend/src/main/java/swith/backend/domain/User.java
@@ -1,7 +1,74 @@
 package swith.backend.domain;
 
 
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User implements UserDetails {
 
 
-public class User {
+    @Id @GeneratedValue
+    private Long id;
+
+
+
+    private String name;
+    private String nickname;
+    private String password;
+    private String serialNumber;
+    private String phoneNumber;
+    private String email;
+
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Builder.Default
+    private List<String> roles = new ArrayList<>();
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
 }
