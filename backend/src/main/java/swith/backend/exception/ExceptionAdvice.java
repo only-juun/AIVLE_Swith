5fcd7edbec10012dcccd9bdc7e95b065351a04d2
leehanjun506 <sis03017@naver.com>
2023-06-30 13:10:35 +0900

    fix - exceptionadvice 중복

--- backend/src/main/java/swith/backend/exception/ExceptionAdvice.java
+++ backend/src/main/java/swith/backend/exception/ExceptionAdvice.java
@@ -10,7 +10,7 @@ import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
+//@RestControllerAdvice
 @Slf4j
 public class ExceptionAdvice {
 
